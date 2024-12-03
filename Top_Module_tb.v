127d1e74483bdb3c55cb24daea66a955e357360c
opw226 <opw226@lehigh.edu>
2024-12-03 16:06:59 -0500

    Add files via upload

--- Top_Module_tb.v
+++ Top_Module_tb.v
@@ -21,12 +21,22 @@
 
 
 module Top_Module_tb();
-    reg clk, rst;
+    reg clk, en;
     reg [3:0] a, b;
     reg [2:0] opp;
     wire [3:0] sseg_a_o;
     wire [6:0] sseg_c_o;
     
-    Top_module uut1(.clk(clk), .rst(rst), .a(a), .b(b), .opp(opp), .sseg_a_o(sseg_a_o), .sseg_c_o(sseg_c_o));
+    Top_Module uut1(.clk(clk), .a(a), .b(b), .en(en), .opp(opp), .sseg_a_o(sseg_a_o), .sseg_c_o(sseg_c_o));
+    
+    initial begin
+        clk = 0;
+        a = 4'b001; b = 4'b0001; opp = 3'b000;
+        #50;
+        en = 1'b1;
+        forever #10 clk = ~clk;
+    end
+    
+    
     
 endmodule
